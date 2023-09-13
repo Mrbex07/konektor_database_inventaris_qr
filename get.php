82f99e72cec92167ee7671111aef94f8bd8843f1
Mrbex07 <hudaibex@gmail.com>
2023-09-13 10:00:43 +0700

    first commit

---
+++ get.php
@@ -0,0 +1,46 @@
+<?php
+require("koneksi.php");
+
+$response = array();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	
+	$id = $_POST['id'];
+	$perintah = "SELECT * FROM barangservice WHERE id = '$id'";
+	$eksekusi = mysqli_query($konek, $perintah);
+	$cek = mysqli_affected_rows($konek);
+
+	if ($cek > 0) {
+		$response["kode"] = 1;
+		$response["pesan"] = "Data Tersedia";
+		$response["data"] = array();
+
+		while ($ambil = mysqli_fetch_object($eksekusi)) {
+			$F["id"] = $ambil->id;
+			$F["kode_barang"] = $ambil->kode_barang;
+			$F["nama_barang"] = $ambil->nama_barang;
+			$F["diterima_oleh"] = $ambil->diterima_oleh;
+			$F["tgl_masuk"] = $ambil->tgl_masuk;
+			$F["tgl_keluar"] = $ambil->tgl_keluar;
+			$F["ket_keluhan"] = $ambil->ket_keluhan;
+			$F["ket_penanganan"] = $ambil->ket_penanganan;
+			$F["divisi"] = $ambil->divisi;
+			$F["anydesk"] = $ambil->anydesk;
+			$F["ket_email"] = $ambil->ket_email;
+			$F["kp_name"] = $ambil->kp_name;
+			$F["hostname"] = $ambil->hostname;
+
+			array_push($response["data"],$F);
+		}
+	}
+	else{
+		$response["kode"] = 0;
+		$response["pesan"] = "data tidak tersedia";
+	}
+}
+else{
+	$response["kode"] = 0;
+	$response["pesan"] = "tidak ada post data";
+}
+echo json_encode($response);
+mysqli_close($konek);
---
+++ koneksi.php
@@ -0,0 +1,6 @@
+<?php 
+ $host = "localhost";
+ $user = "root";
+ $pass = "";
+ $db = "inventaris9";
+ $konek = mysqli_connect($host, $user, $pass, $db) or die("Databse tidak terhubung");
